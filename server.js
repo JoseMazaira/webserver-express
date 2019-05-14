8691a35c3f8f6faced5614706d096eeb8ebaad7c
Jose Mazaira <jose.mazaira@redegal.com>
2019-05-14 12:40:53 +0200

    Primer commit

---
+++ server.js
@@ -0,0 +1,39 @@
+/**
+ * Servidor de ejemplo del curso de Node.js
+ */
+
+const express = require('express');
+const app = express();
+
+
+const hbs = require('hbs');
+require('./hbs/helpers')
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+//Express HBS engine
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+//Helpers
+
+app.get('/', (req, res) => {
+
+
+    res.render('home', {
+        nombre: 'jose maZaira fernandez',
+        year: new Date().getFullYear()
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        year: new Date().getFullYear()
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${port}`);
+});
